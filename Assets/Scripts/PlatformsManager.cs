dc752e17b79a48e0c0611a7d91e0b50241c0fe82
Isaac <isaac.l@uteca.edu.mx>
2026-04-16 20:57:02 -0600

    EXAMEN3

--- Assets/Scripts/PlatformsManager.cs
+++ Assets/Scripts/PlatformsManager.cs
@@ -52,6 +52,7 @@ public class PlatformsManager : MonoBehaviour
     {
         InstantiatePoolObjects instantiatePool = flyingPlatformPrefabs[Random.Range(0, flyingPlatformPrefabs.Length)];
         Vector3 spawnPosition = character.position - transform.position + Vector3.forward * 2f;
+        spawnPosition.x = 0f;
         instantiatePool.InstantiateObject(spawnPosition);
         GameObject createdPlatform = instantiatePool.GetCurrentObject();
         Platform newPlatform = createdPlatform.GetComponent<Platform>();
